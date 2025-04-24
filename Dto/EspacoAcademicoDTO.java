7e209e83f45ca69784db46e63057216c3229f4c7
yurifr-dev <143610076+yurifr-dev@users.noreply.github.com>
2025-04-23 21:10:12 -0300

    Create EspacoAcademicoDTO.java

---
+++ Dto/EspacoAcademicoDTO.java
@@ -0,0 +1,9 @@
+package com.ucsal.arqui_back.dto;
+
+public class EspacoAcademicoDTO {
+    private String sigla;
+    private String nome;
+    private String descricao;
+    private Integer capacidade;
+    private String tipo;
+}
