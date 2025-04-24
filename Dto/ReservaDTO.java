2ca2942ed08f23812beed71a25be44810def3462
yurifr-dev <143610076+yurifr-dev@users.noreply.github.com>
2025-04-23 21:10:48 -0300

    Create ReservaDTO.java

---
+++ Dto/ReservaDTO.java
@@ -0,0 +1,7 @@
+public class ReservaDTO {
+    private String professor;
+    private Long espacoId;
+    private LocalDate data;
+    private LocalTime horarioInicio;
+    private LocalTime horarioFim;
+}
